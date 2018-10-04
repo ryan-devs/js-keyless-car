962aa0bbf42da96043d2dd1a9f0c4c1033831d4f
ryandevs <mnp1@nym.hush.com>
2018-10-04 13:42:02 -0500

    created keyless car js app

---
+++ main.js
@@ -0,0 +1,9 @@
+"use strict";
+
+let age = prompt("How old are you?");
+if(age == 18)
+    {alert("Congratulations on your first year of driving. Enjoy the ride!");
+}
+else {    
+(age > 18) ? alert("Powering On. Enjoy the ride!") : alert("Sorry, you are too young to drive this car. Powering off");
+}
